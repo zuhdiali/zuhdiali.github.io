ca47a9675bce184a4da3522bfa8cbd7fc2cdfcd4
zuhdiali <zuhdialihisyam@gmail.com>
2026-04-12 12:51:53 +0700

    update on navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,37 @@
   /**
    * Header toggle
    */
+  const headerToggleBtn = document.querySelector(".header-toggle");
+
+  function headerToggle() {
+    document.querySelector("#header").classList.toggle("header-show");
+    headerToggleBtn.classList.toggle("bi-list");
+    headerToggleBtn.classList.toggle("bi-x");
+  }
+  headerToggleBtn.addEventListener("click", headerToggle);
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll("#navmenu a").forEach((navmenu) => {
+    navmenu.addEventListener("click", () => {
+      if (document.querySelector(".header-show")) {
+        headerToggle();
+      }
+    });
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll(".navmenu .toggle-dropdown").forEach((navmenu) => {
+    navmenu.addEventListener("click", function (e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle("active");
+      this.parentNode.nextElementSibling.classList.toggle("dropdown-active");
+      e.stopImmediatePropagation();
+    });
+  });
 
   /**
    * Preloader
@@ -121,7 +152,7 @@
           layoutMode: layout,
           filter: filter,
           sortBy: sort,
-        }
+        },
       );
     });
 
@@ -142,7 +173,7 @@
               aosInit();
             }
           },
-          false
+          false,
         );
       });
   });
@@ -153,7 +184,7 @@
   function initSwiper() {
     document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
       let config = JSON.parse(
-        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+        swiperElement.querySelector(".swiper-config").innerHTML.trim(),
       );
 
       if (swiperElement.classList.contains("swiper-tab")) {
